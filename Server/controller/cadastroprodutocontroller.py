08f572f8f824adc22c97e074afdcfbc24000cd23
Luiz Felipe <luizfelipenascimentogomes@outlook.com>
2024-05-24 04:13:09 -0300

    Feat: cadastroprodutocontroller

---
+++ Server/controller/cadastroprodutocontroller.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+Produto = []
+
+@app.route('/Produto', methods=['POST'])
+def create_Produto():
+    Produto_data = request.json
+    new_Produto = Produto(
+        name=Produto_data['name'],
+        price=Produto_data['price'],
+        promo_price=Produto_data['promo_price'],
+        quantity=Produto_data['quantity'],
+        description=Produto_data['description'],
+        images=Produto_data['images'],
+        variations=Produto_data['variations']
+    )
+    Produto.append(new_Produto.to_dict())
+    return jsonify(new_Produto.to_dict()), 201
+
+@app.route('/Produto/<int:Produto_id>', methods=['PUT'])
+def update_Produto(Produto_id):
+    Produto_data = request.json
+    for Produto in Produto:
+        if Produto['id'] == Produto_id:
+            Produto.update(Produto_data)
+            return jsonify(Produto)
+    return jsonify({'message': 'Produto não encontrado'}), 404
+
+@app.route('/Produto/<int:Produto_id>', methods=['DELETE'])
+def delete_Produto(Produto_id):
+    global Produto
+    Produto = [Produto for Produto in Produto if Produto['id'] != Produto_id]
+    return jsonify({'message': 'Produto excluído com sucesso'}), 200
