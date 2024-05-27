2b4c5095214105c0804a268d64972ab7fdb11101
Luiz Felipe <luizfelipenascimentogomes@outlook.com>
2024-05-27 00:33:08 -0300

    Feat: model do Usuário

--- Server/models/model.py
+++ Server/models/model.py
@@ -1,4 +1,4 @@
-from app import db
+from __init__ import db
 
 class Vendedor(db.Model):
     id_vendedor = db.Column(db.Integer, primary_key=True)
@@ -38,4 +38,32 @@ class Produto(db.Model):
             'fk_id_vendedor': self.fk_id_vendedor,
             'has_ativo': self.has_ativo,
             'data_criacao': self.data_criacao
+        }
+
+class User(db.Model):
+    id_usuario = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), nullable=False)
+    senha = db.Column(db.String(12), nullable=False)
+    nome_usuario = db.Column(db.String(15), unique=True, nullable=False)
+    nome_pessoa = db.Column(db.String(255), nullable=False)
+    cpf = db.Column(db.String(11), unique=True, nullable=False)
+    telefone = db.Column(db.String(14))
+    cep = db.Column(db.String(8))
+    endereco = db.Column(db.String(255))
+    complemento = db.Column(db.String(255))
+    has_ativo = db.Column(db.Boolean, nullable=False, default=True)
+    data_cadastro = db.Column(db.TIMESTAMP, nullable=False, default=db.func.current_timestamp())
+
+    def to_dict(self):
+        return {
+            'id_usuario': self.id_usuario,
+            'email': self.email,
+            'senha': self.senha,
+            'nome_usuario': self.nome_pessoa,
+            'nome_pessoa': self.nome_pessoa,
+            'cpf': self.cpf,
+            'endereco': self.endereco,
+            'complemento': self.endereco,
+            'has_ativo': self.has_ativo,
+            'data_cadastro': self.data_cadastro
         }
