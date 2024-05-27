af85ce27c7136143514f738423ab3a4ff6cb3ca2
Luiz Felipe <luizfelipenascimentogomes@outlook.com>
2024-05-26 23:12:41 -0300

    Feat: auth controller

---
+++ Server/controller/auth_controller.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import create_access_token
+from models import User, Vendedor
+from app import db
+
+auth_bp = Blueprint('auth_bp', __name__)
+
+@auth_bp.route('/auth/login', methods=['POST'])
+def Login():
+    data = request.json
+    user = User.query.filter_by(username=data['username']).first()
+    if user and user.check_password(data['password']):
+        access_token = create_access_token(identity=user.id)
+        return jsonify(access_token=access_token), 200
+    return jsonify({'messages': 'Dados inválidos'}), 401
